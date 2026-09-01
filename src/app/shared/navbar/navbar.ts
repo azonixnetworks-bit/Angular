ab893fb292eb51e31e7bf9545cb96c7ac3044b60
mohamedsihan1995Dev <mohamedsihan@gmail.com>
2026-09-01 16:47:55 +0530

    feat: add cart service and cart integration

--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -1,4 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal ,inject  } from '@angular/core';
+import { CartService } from '../../core/services/cart';
+
 
 @Component({
   selector: 'app-navbar',
@@ -7,11 +9,16 @@ import { Component, signal } from '@angular/core';
   styleUrl: './navbar.scss',
 })
 export class Navbar {
-
+private cartService = inject(CartService);
   cartCount = signal(0);
 
   addToCart() {
     this.cartCount.update(count => count + 1);
+     return this.cartService.getItemCount();
   }
+
+ 
+ 
+
 
 }
